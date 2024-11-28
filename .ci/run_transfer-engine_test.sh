b048aaae3a6c50c9704424c4f449941e2dcb5748
zhangxinyi <zhangxinyi@linux.alibaba.com>
2024-11-28 14:21:06 +0800

    skip rdma

--- .ci/run_transfer-engine_test.sh
+++ .ci/run_transfer-engine_test.sh
@@ -22,16 +22,17 @@ etcd --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://10.
 export MC_GID_INDEX=1
 run_test transport_uint_test
 run_test tcp_transport_test
+run_test transfer_metadata_test
 export MC_GID_INDEX=1
 
 # sudo pkill etcd
 # sleep 1
 # etcd --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://10.0.0.1:2379 &
 # sudo fuser -k 14345/tcp
-export MC_GID_INDEX=1 && ./rdma_transport_test --mode=target  --metadata_server=127.0.0.1:2379 --local_server_name=127.0.0.2:14345 --device_name=erdma_0 &
-export MC_GID_INDEX=1 && ./rdma_transport_test --metadata_server=127.0.0.1:2379 --segment_id=127.0.0.2:14345 --local_server_name=127.0.0.3:14346 --device_name=erdma_1
-LOCAL_SUCCESS=$?
-parse_result $LOCAL_SUCCESS "rdma_transport_test"
+#export MC_GID_INDEX=1 && ./rdma_transport_test --mode=target  --metadata_server=127.0.0.1:2379 --local_server_name=127.0.0.2:14345 --device_name=erdma_0 &
+#export MC_GID_INDEX=1 && ./rdma_transport_test --metadata_server=127.0.0.1:2379 --segment_id=127.0.0.2:14345 --local_server_name=127.0.0.3:14346 --device_name=erdma_1
+#LOCAL_SUCCESS=$?
+#parse_result $LOCAL_SUCCESS "rdma_transport_test"
 
 # sudo pkill etcd
 # sleep 1
@@ -39,7 +40,7 @@ parse_result $LOCAL_SUCCESS "rdma_transport_test"
 # export MC_GID_INDEX=1
 # sudo fuser -k 15345/tcp
 # export MC_GID_INDEX=1 && ./rdma_transport_test --mode=target  --metadata_server=127.0.0.1:2379 --local_server_name=127.0.0.2:14345 --device_name=erdma_0 &
-export MC_GID_INDEX=1 && ./rdma_transport_test2 --metadata_server=127.0.0.1:2379 --segment_id=127.0.0.2:14345 --local_server_name=127.0.0.3:14346 --device_name=erdma_1
-LOCAL_SUCCESS=$?
-parse_result $LOCAL_SUCCESS "rdma_transport_test2"
+#export MC_GID_INDEX=1 && ./rdma_transport_test2 --metadata_server=127.0.0.1:2379 --segment_id=127.0.0.2:14345 --local_server_name=127.0.0.3:14346 --device_name=erdma_1
+#LOCAL_SUCCESS=$?
+#parse_result $LOCAL_SUCCESS "rdma_transport_test2"
 popd
